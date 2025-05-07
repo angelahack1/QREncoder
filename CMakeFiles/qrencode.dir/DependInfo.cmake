b62a5fdc4a4bb0411ed42876a3e1f890d576a46b
Angela López Mendoza <angelakimichellle@hotmail.com>
2025-05-06 18:55:59 -0600

    Just Initial Commit, by angelahack1

---
+++ CMakeFiles/qrencode.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/e/libqrencode-4.1.1/bitstream.c" "CMakeFiles/qrencode.dir/bitstream.c.o" "gcc" "CMakeFiles/qrencode.dir/bitstream.c.o.d"
+  "/mnt/e/libqrencode-4.1.1/mask.c" "CMakeFiles/qrencode.dir/mask.c.o" "gcc" "CMakeFiles/qrencode.dir/mask.c.o.d"
+  "/mnt/e/libqrencode-4.1.1/mmask.c" "CMakeFiles/qrencode.dir/mmask.c.o" "gcc" "CMakeFiles/qrencode.dir/mmask.c.o.d"
+  "/mnt/e/libqrencode-4.1.1/mqrspec.c" "CMakeFiles/qrencode.dir/mqrspec.c.o" "gcc" "CMakeFiles/qrencode.dir/mqrspec.c.o.d"
+  "/mnt/e/libqrencode-4.1.1/qrencode.c" "CMakeFiles/qrencode.dir/qrencode.c.o" "gcc" "CMakeFiles/qrencode.dir/qrencode.c.o.d"
+  "/mnt/e/libqrencode-4.1.1/qrinput.c" "CMakeFiles/qrencode.dir/qrinput.c.o" "gcc" "CMakeFiles/qrencode.dir/qrinput.c.o.d"
+  "/mnt/e/libqrencode-4.1.1/qrspec.c" "CMakeFiles/qrencode.dir/qrspec.c.o" "gcc" "CMakeFiles/qrencode.dir/qrspec.c.o.d"
+  "/mnt/e/libqrencode-4.1.1/rsecc.c" "CMakeFiles/qrencode.dir/rsecc.c.o" "gcc" "CMakeFiles/qrencode.dir/rsecc.c.o.d"
+  "/mnt/e/libqrencode-4.1.1/split.c" "CMakeFiles/qrencode.dir/split.c.o" "gcc" "CMakeFiles/qrencode.dir/split.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
